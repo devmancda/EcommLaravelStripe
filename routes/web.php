137b0640a8987b762cb988c94a168e7429858130
Harry <harrybourgeois@gmail.com>
2021-10-04 17:04:31 +0200

    Creation config du projet(commit_main)

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Auth & gestion compte et commandes
+
+// La boutique & gestion des intl_get_error_message
+
+// Panier d'achat
+
+// Paiement
+
