4129da4d635e63338c56e3d678cd967357ae5c69
Harry <harrybourgeois@gmail.com>
2021-10-04 21:32:40 +0200

    Fin Policies

---
+++ app/Policies/OrderPolicy.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Order;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class OrderPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the order.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Order  $order
+     * @return mixed
+     */
+    public function view(User $user, Order $order)
+    {
+        //
+    }
+
+}
