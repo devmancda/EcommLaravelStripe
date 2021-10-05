f0e13bccbd9fd9be7600e3fea5e88ace902d5d93
Harry <harrybourgeois@gmail.com>
2021-10-05 13:04:34 +0200

    Gestion des routes pour la boutique

--- routes/web.php
+++ routes/web.php
@@ -11,19 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // Auth & gestion compte et commandes
 
+use App\Http\Controllers\ItemController;
+
+Auth::routes();
+
+Route::get('/compte', 'HomeController@index')->name('home');
+
 // La boutique & gestion des intl_get_error_message
+
+Route::get('/', 'ItemController@index')->name('item.index');
+Route::resource('item', 'ItemController')->except('index', 'show', 'create');
 
 // Panier d'achat
 
 // Paiement
-
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
