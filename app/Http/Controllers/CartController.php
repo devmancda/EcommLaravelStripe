0e34a3e623be353d1f1bd60657854074af449db1
Harry <harrybourgeois@gmail.com>
2021-10-05 12:58:54 +0200

    Creation CartController

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Cart;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+}
